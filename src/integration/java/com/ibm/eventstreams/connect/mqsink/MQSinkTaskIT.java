b091b891747f8471553cd0c7d3cf93fe58c22a0c
A S Adil Mohammad <AdilMohammad@ibm.com>
2025-07-18 21:26:33 +0530

    fix: incorrect datatype in config Signed-off-by: A S Adil Mohammad <asadilmohammad2020@gmail.com>

--- src/integration/java/com/ibm/eventstreams/connect/mqsink/MQSinkTaskIT.java
+++ src/integration/java/com/ibm/eventstreams/connect/mqsink/MQSinkTaskIT.java
@@ -128,6 +128,44 @@ public class MQSinkTaskIT extends AbstractJMSContextIT {
     }
 
     @Test
+    public void verifyMqConfig() throws JMSException {
+        final MQSinkTask newConnectTask = new MQSinkTask();
+
+        // configure a sink task for string messages
+        final Map<String, String> connectorConfigProps = createDefaultConnectorProperties();
+        connectorConfigProps.put("mq.message.builder",
+                DEFAULT_MESSAGE_BUILDER);
+
+        connectorConfigProps.put("mq.ssl.use.ibm.cipher.mappings",
+                "true");        
+
+        // start the task so that it connects to MQ
+        newConnectTask.start(connectorConfigProps);
+
+        // create some test messages
+        final List<SinkRecord> records = new ArrayList<>();
+        records.add(generateSinkRecord(null, "hello"));
+        records.add(generateSinkRecord(null, "world"));
+        newConnectTask.put(records);
+
+        // flush the messages
+        final Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
+        final TopicPartition topic = new TopicPartition(TOPIC, PARTITION);
+        final OffsetAndMetadata offset = new OffsetAndMetadata(commonOffset);
+        offsets.put(topic, offset);
+        newConnectTask.flush(offsets);
+
+        // stop the task
+        newConnectTask.stop();
+
+        // verify that the messages were successfully submitted to MQ
+        final List<Message> messagesInMQ = getAllMessagesFromQueue(DEFAULT_SINK_QUEUE_NAME);
+        assertEquals(2, messagesInMQ.size());
+        assertEquals("hello", messagesInMQ.get(0).getBody(String.class));
+        assertEquals("world", messagesInMQ.get(1).getBody(String.class));
+    }
+
+    @Test
     public void verifyStringJmsMessages() throws JMSException {
         final MQSinkTask newConnectTask = new MQSinkTask();
 
